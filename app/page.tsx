1fd822b60600f773d89b46674a969dc9390203df
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-06-12 10:59:21 +0100

    link source code

--- app/page.tsx
+++ app/page.tsx
@@ -19,19 +19,11 @@ export default function Home() {
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Janarth Duraisingham {" "} <br />
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://github.com/janarthduraisingham/chess"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+              Source code
+            </a>
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
