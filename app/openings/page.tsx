b1d469123abf0b655e4c5a199f92c22190b3847e
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-06-12 11:52:30 +0100

    remove picture from openings page

--- app/openings/page.tsx
+++ app/openings/page.tsx
@@ -4,14 +4,6 @@ export default function Openings() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-stone-600">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-stone-600 sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/chessboard.jpg"
-          alt="chessboard"
-          width={100}
-          height={20}
-          priority
-        />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Openings.
