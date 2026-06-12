bcb0b91a1b75f02feb7787115f0e601dffc3402a
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-06-12 13:48:50 +0100

    add link to Openings page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
+import Link from 'next/link';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <nav>
+          <Link href='/openings'>Openings</Link>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
