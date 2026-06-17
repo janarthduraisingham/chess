a287220058d889e00ffd07926dfa38de6af70f13
janarthduraisingham <51697690+janarthduraisingham@users.noreply.github.com>
2026-06-17 17:31:41 +0100

    remove name from homepage (#29)

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             Welcome to chessmate <br /> <span className='font-bold text-4xl'>Your move.</span>
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Janarth Duraisingham {" "} <br />
+            {/* Janarth Duraisingham {" "} <br /> */}
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
